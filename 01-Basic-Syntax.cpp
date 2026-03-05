ff388c0213a80e10db19b588d934b1b5ea78647f
Divyanshu <divyanshu473b@gmail.com>
2026-03-05 18:14:35 +0530

    Learned Basic syntax and types

---
+++ 01-Basic-Syntax.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    // Data types 
+    int integer = 5;
+    long long largeInteger = 100000000000LL;
+    double decimalNum = 23.234234;
+    char singleChar = 'B';
+    std::string longChars = "Hello how are you doing";
+    bool boolean = true;
+    
+    // type casting
+    int a = 5;
+    int b = 2;
+    double result = (double) 5/2;
+    // std::cout << result;
+
+    return 0;
+}
